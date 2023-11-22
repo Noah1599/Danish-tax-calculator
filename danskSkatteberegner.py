64fc1861e44bc430196b65bc0933e0183731a324
Noah1599 <115689277+Noah1599@users.noreply.github.com>
2023-11-22 21:17:36 +0100

    Update danskSkatteberegner.py
    bedre læsbart

--- danskSkatteberegner.py
+++ danskSkatteberegner.py
@@ -1,46 +1,48 @@
-#løn
-timeLøn=131.4
-timerArbejdetOmDagen=8
-dageArbejdeUgen=5
-ugerOmÅret=52
+# Løn
+time_løn = 654.4
+timer_arbejdet_om_dagen = 8
+dage_arbejde_ugen = 5
+uger_om_året = 52
 
-#satser
-su=1004
-bundSkat=0.1206
-komuneskat=0.234
-kirkeskat=0.0089
-personFradrag=49700
-amBidrag=0.08
-skatteLoft=0.5207
-topSkatStarter=588900
-topSkat=0.15
+# Skattesatser
+su = 1004
+grundskat_sats = 0.1206
+kommuneskat_sats = 0.234
+kirkeskat_sats = 0.0089
+personligt_fraktræk = 49700
+am_bidrag_sats = 0.08
+skatte_loft = 0.5207
+topskat_starter = 588900
+topskat_sats = 0.15
 
+# Nu starter programmet
+betalt_skat = grundskat_sats + kommuneskat_sats + kirkeskat_sats
+maksimal_skat_sats = betalt_skat + topskat_sats
+total_timer = timer_arbejdet_om_dagen * dage_arbejde_ugen * uger_om_året
+brutto_løn = total_timer * time_løn + su * 12
 
-#now the program starts
-betalesSkat=bundSkat+komuneskat+kirkeskat
-maxSkat=betalesSkat+topSkat
-totalTimer=timerArbejdetOmDagen*dageArbejdeUgen*ugerOmÅret
-bruttoLon=totalTimer*timeLøn+su*12
-print("skat før topskat er: ",betalesSkat*100,"%")
-print("skat efter topskat er: ",maxSkat*100,"%\n")
-print("total timer arbejdet om året: ",totalTimer,"og brutto løn er: ",bruttoLon,"kr")
-udenAmBidrag=bruttoLon-(bruttoLon*amBidrag)
-udenPersonFradrag=udenAmBidrag-personFradrag
-betalerViTopskat=udenPersonFradrag>topSkatStarter
-if betalerViTopskat:
-    topskatBetalesAf=udenPersonFradrag-topSkatStarter
-    print("\n\ntopskat betales af: ",int(topskatBetalesAf),"kr")
-    PengeMedNormalSkat=topSkatStarter-(topSkatStarter*betalesSkat)
-    print("Normal skat betales af: ",int(topSkatStarter),"kr")
-    print("\nTopskat blevet betalt",int(topskatBetalesAf*maxSkat),"kr")
-    print("Normal skat blevet betalt: ",int(topSkatStarter*betalesSkat),"kr\n")
-    topSkatBetales=topskatBetalesAf-(topskatBetalesAf*maxSkat)
-    pengeEfterSkat=PengeMedNormalSkat+topSkatBetales
-    print("penge efter skat: ",int(pengeEfterSkat),"kr")
-    print("\npenge betalt i skat: ",int(bruttoLon-pengeEfterSkat),"kr")
-    print("penge efter skat: ",int(pengeEfterSkat),"kr")
+print("Skat før topskat er: ", betalt_skat * 100, "%")
+print("Skat efter topskat er: ", maksimal_skat_sats * 100, "%\n")
+print("Total timer arbejdet om året: ", total_timer, "og brutto løn er: ", brutto_løn, "kr")
+
+uden_am_bidrag = brutto_løn - (brutto_løn * am_bidrag_sats)
+uden_personligt_fraktræk = uden_am_bidrag - personligt_fraktræk
+betaler_topskat = uden_personligt_fraktræk > topskat_starter
+
+if betaler_topskat:
+    topskat_betalt_af = uden_personligt_fraktræk - topskat_starter
+    print("\n\ntopskat betales af: ", int(topskat_betalt_af), "kr")
+    penge_med_normal_skat = topskat_starter - (topskat_starter * betalt_skat)
+    print("Normal skat betales af: ", int(topskat_starter), "kr")
+    print("\nTopskat blevet betalt", int(topskat_betalt_af * maksimal_skat_sats), "kr")
+    print("Normal skat blevet betalt: ", int(topskat_starter * betalt_skat), "kr\n")
+    topskat_betalt = topskat_betalt_af - (topskat_betalt_af * maksimal_skat_sats)
+    penge_efter_skat = penge_med_normal_skat + topskat_betalt
+    print("Penge efter skat: ", int(penge_efter_skat), "kr")
+    print("\nPenge betalt i skat: ", int(brutto_løn - penge_efter_skat), "kr")
+    print("Penge efter skat: ", int(penge_efter_skat), "kr")
 else:
-    pengeEfterSkat=udenPersonFradrag-(udenPersonFradrag*betalesSkat)
-    print("penge betalt i skat: ",int(udenPersonFradrag*betalesSkat),"kr")
-    print("\npenge efter skat: ",int(pengeEfterSkat),"kr")
-    print("betalte penge i skat: ",int(udenPersonFradrag*betalesSkat),"kr")
+    penge_efter_skat = uden_personligt_fraktræk - (uden_personligt_fraktræk * betalt_skat)
+    print("Penge betalt i skat: ", int(uden_personligt_fraktræk * betalt_skat), "kr")
+    print("\nPenge efter skat: ", int(penge_efter_skat), "kr")
+    print("Betalte penge i skat: ", int(uden_personligt_fraktræk * betalt_skat), "kr")
