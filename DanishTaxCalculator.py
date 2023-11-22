d9f755d4defb00cea0f2bdb76ea9dbd29b3f7ebc
Noah1599 <115689277+Noah1599@users.noreply.github.com>
2023-11-22 21:16:57 +0100

    Update DanishTaxCalculator.py
    now in english

--- DanishTaxCalculator.py
+++ DanishTaxCalculator.py
@@ -1,52 +1,48 @@
-  #løn
-timeLøn=700
-timerArbejdetOmDagen=8
-dageArbejdeUgen=5
-ugerOmÅret=52
+# Salary
+hourly_wage = 131.4
+hours_worked_per_day = 8
+days_worked_per_week = 5
+weeks_per_year = 52
 
-#satser
-bundSkat=0.1206
-komuneskat=0.234
-kirkeskat=0.0089
-personFradrag=49700
-amBidrag=0.08
-#skatte loft er kommuneskat + bundskat + topskat ikke am bidrag og kirkeskat
-skatteLoft=0.5207
-#husk dev topskat bundkrense efter am bidrag fratrukket
-topSkatStarter=588900
-topSkat=0.15
+# Rates
+su = 1004
+basic_tax_rate = 0.1206
+municipal_tax_rate = 0.234
+church_tax_rate = 0.0089
+personal_deduction = 49700
+am_contribution_rate = 0.08
+tax_ceiling = 0.5207
+top_tax_threshold = 588900
+top_tax_rate = 0.15
 
+# Now the program starts
+tax_paid = basic_tax_rate + municipal_tax_rate + church_tax_rate
+max_tax_rate = tax_paid + top_tax_rate
+total_hours = hours_worked_per_day * days_worked_per_week * weeks_per_year
+gross_salary = total_hours * hourly_wage + su * 12
 
-#now the program starts
-betalesSkat=bundSkat+komuneskat+kirkeskat
-maxSkat=betalesSkat+topSkat
-totalTimer=timerArbejdetOmDagen*dageArbejdeUgen*ugerOmÅret
-bruttoLon=totalTimer*timeLøn
-print("skat før topskat er: ",betalesSkat*100,"%")
-print("skat efter topskat er: ",maxSkat*100,"%\n")
-print("total timer arbejdet om året: ",totalTimer,"og brutto løn er: ",bruttoLon,"kr")
-#print("betalt am bidrag: ",int(bruttoLon*amBidrag),"kr")
-udenAmBidrag=bruttoLon-(bruttoLon*amBidrag)
-#print("uden am bidrag: ",int(udenAmBidrag),"kr")
-udenPersonFradrag=udenAmBidrag-personFradrag
-#print("uden person fradrag: ",int(udenPersonFradrag),"kr")
-betalerViTopskat=udenPersonFradrag>topSkatStarter
-#print("betaler vi topskat: ",betalerViTopskat)
-if betalerViTopskat:
-    topskatBetalesAf=udenPersonFradrag-topSkatStarter
-    print("\n\ntopskat betales af: ",int(topskatBetalesAf),"kr")
-    PengeMedNormalSkat=topSkatStarter-(topSkatStarter*betalesSkat)
-    print("Normal skat betales af: ",int(topSkatStarter),"kr")
-    print("\nTopskat blevet betalt",int(topskatBetalesAf*maxSkat),"kr")
-    print("Normal skat blevet betalt: ",int(topSkatStarter*betalesSkat),"kr\n")
-    topSkatBetales=topskatBetalesAf-(topskatBetalesAf*maxSkat)
-    pengeEfterSkat=PengeMedNormalSkat+topSkatBetales
-    print("penge efter skat: ",int(pengeEfterSkat),"kr")
-    print("\npenge betalt i skat: ",int(bruttoLon-pengeEfterSkat),"kr")
-    print("penge efter skat: ",int(pengeEfterSkat),"kr")
+print("Tax before top tax is: ", tax_paid * 100, "%")
+print("Tax after top tax is: ", max_tax_rate * 100, "%\n")
+print("Total hours worked per year: ", total_hours, "and gross salary is: ", gross_salary, "kr")
+
+without_am_contribution = gross_salary - (gross_salary * am_contribution_rate)
+without_personal_deduction = without_am_contribution - personal_deduction
+pays_top_tax = without_personal_deduction > top_tax_threshold
+
+if pays_top_tax:
+    top_tax_paid_by = without_personal_deduction - top_tax_threshold
+    print("\n\nTop tax paid by: ", int(top_tax_paid_by), "kr")
+    normal_tax_paid = top_tax_threshold - (top_tax_threshold * tax_paid)
+    print("Normal tax paid by: ", int(top_tax_threshold), "kr")
+    print("\nTop tax paid: ", int(top_tax_paid_by * max_tax_rate), "kr")
+    print("Normal tax paid: ", int(top_tax_threshold * tax_paid), "kr\n")
+    top_tax_paid = top_tax_paid_by - (top_tax_paid_by * max_tax_rate)
+    money_after_tax = normal_tax_paid + top_tax_paid
+    print("Money after tax: ", int(money_after_tax), "kr")
+    print("\nMoney paid in tax: ", int(gross_salary - money_after_tax), "kr")
+    print("Money after tax: ", int(money_after_tax), "kr")
 else:
-    pengeEfterSkat=udenPersonFradrag-(udenPersonFradrag*betalesSkat)
-    print("penge betalt i skat: ",int(udenPersonFradrag*betalesSkat),"kr")
-    print("\npenge efter skat: ",int(pengeEfterSkat),"kr")
-    print("betalte penge i skat: ",int(udenPersonFradrag*betalesSkat),"kr")
-
+    money_after_tax = without_personal_deduction - (without_personal_deduction * tax_paid)
+    print("Money paid in tax: ", int(without_personal_deduction * tax_paid), "kr")
+    print("\nMoney after tax: ", int(money_after_tax), "kr")
+    print("Paid money in tax: ", int(without_personal_deduction * tax_paid), "kr")
