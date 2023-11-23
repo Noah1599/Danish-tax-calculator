6c99421dee6daadcc5dc064db01c170ce3ed8415
Noah1599 <115689277+Noah1599@users.noreply.github.com>
2023-11-23 09:52:12 +0100

    Update DanishTaxCalculator.py
    correct deduction

--- DanishTaxCalculator.py
+++ DanishTaxCalculator.py
@@ -1,48 +1,53 @@
 # Salary
 hourly_wage = 131.4
-hours_worked_per_day = 8
-days_worked_per_week = 5
+hours_worked_per_day = 3
+days_work_per_week = 2
 weeks_per_year = 52
 
-# Rates
-su = 1004
+# Tax Rates
+su = 0
 basic_tax_rate = 0.1206
-municipal_tax_rate = 0.234
+municipal_tax_rate = 0.2552
 church_tax_rate = 0.0089
 personal_deduction = 49700
-am_contribution_rate = 0.08
+employment_contribution_rate = 0.08
 tax_ceiling = 0.5207
 top_tax_threshold = 588900
 top_tax_rate = 0.15
 
-# Now the program starts
-tax_paid = basic_tax_rate + municipal_tax_rate + church_tax_rate
-max_tax_rate = tax_paid + top_tax_rate
-total_hours = hours_worked_per_day * days_worked_per_week * weeks_per_year
+# The program starts here
+paid_tax = basic_tax_rate + municipal_tax_rate + church_tax_rate
+maximum_tax_rate = paid_tax + top_tax_rate
+total_hours = hours_worked_per_day * days_work_per_week * weeks_per_year
 gross_salary = total_hours * hourly_wage + su * 12
-
-print("Tax before top tax is: ", tax_paid * 100, "%")
-print("Tax after top tax is: ", max_tax_rate * 100, "%\n")
+print("Tax before top tax: ", paid_tax * 100, "%")
+print("Tax after top tax: ", maximum_tax_rate * 100, "%\n")
 print("Total hours worked per year: ", total_hours, "and gross salary is: ", gross_salary, "kr")
 
-without_am_contribution = gross_salary - (gross_salary * am_contribution_rate)
-without_personal_deduction = without_am_contribution - personal_deduction
+without_employment_contribution = gross_salary - (gross_salary * employment_contribution_rate)
+if without_employment_contribution < personal_deduction:
+    without_personal_deduction = without_employment_contribution
+else:
+    without_personal_deduction = without_employment_contribution - personal_deduction
 pays_top_tax = without_personal_deduction > top_tax_threshold
 
 if pays_top_tax:
-    top_tax_paid_by = without_personal_deduction - top_tax_threshold
-    print("\n\nTop tax paid by: ", int(top_tax_paid_by), "kr")
-    normal_tax_paid = top_tax_threshold - (top_tax_threshold * tax_paid)
-    print("Normal tax paid by: ", int(top_tax_threshold), "kr")
-    print("\nTop tax paid: ", int(top_tax_paid_by * max_tax_rate), "kr")
-    print("Normal tax paid: ", int(top_tax_threshold * tax_paid), "kr\n")
-    top_tax_paid = top_tax_paid_by - (top_tax_paid_by * max_tax_rate)
-    money_after_tax = normal_tax_paid + top_tax_paid
+    top_tax_paid_on = without_personal_deduction - top_tax_threshold
+    print("\n\ntop tax paid on: ", int(top_tax_paid_on), "kr")
+    money_with_normal_tax = top_tax_threshold - (top_tax_threshold * paid_tax)
+    print("Normal tax paid on: ", int(top_tax_threshold), "kr")
+    print("\nTop tax paid: ", int(top_tax_paid_on * maximum_tax_rate), "kr")
+    print("Normal tax paid: ", int(top_tax_threshold * paid_tax), "kr\n")
+    top_tax_paid = top_tax_paid_on - (top_tax_paid_on * maximum_tax_rate)
+    money_after_tax = money_with_normal_tax + top_tax_paid + personal_deduction
     print("Money after tax: ", int(money_after_tax), "kr")
     print("\nMoney paid in tax: ", int(gross_salary - money_after_tax), "kr")
     print("Money after tax: ", int(money_after_tax), "kr")
 else:
-    money_after_tax = without_personal_deduction - (without_personal_deduction * tax_paid)+personal_deduction
-    print("Money paid in tax: ", int(without_personal_deduction * tax_paid), "kr")
+    print("\nMoney subject to tax: ", int(without_personal_deduction), "kr")
+    if without_personal_deduction < personal_deduction:
+        money_after_tax = without_personal_deduction
+    else:
+        money_after_tax = without_personal_deduction - (without_personal_deduction * paid_tax) + personal_deduction
     print("\nMoney after tax: ", int(money_after_tax), "kr")
-    print("Paid money in tax: ", int(without_personal_deduction * tax_paid), "kr")
+    print("Paid money in tax: ", int(without_personal_deduction - money_after_tax), "kr")
